5b7bfa95d485fe562c40a36e8c05efca8b610a7b
Lingyu Zhou <zly@TRLDSC2.CHBOSTON.ORG>
2019-04-29 14:45:47 -0400

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React, {Component, Fragment} from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component, Fragment} from "react";
+import ReactDOM from "react-dom";
 import {DateXAxis} from "./lib";
 
 class App extends Component {
