7ff1299d60adefd0820c1cca424f317f4a8f01b9
Lingyu Zhou <zhoulytwin@gmail.com>
2019-05-29 15:30:00 -0400

    Updates

---
+++ precache-manifest.d1b830314c3fed9b3d38ef10bca76f13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca52f841a2dca80f08a4c8dcb3162bd",
+    "url": "/react-plot-axis/index.html"
+  },
+  {
+    "revision": "930d0db31cef01270d96",
+    "url": "/react-plot-axis/static/css/main.9d313dd0.chunk.css"
+  },
+  {
+    "revision": "27a81ee07f21236d0643",
+    "url": "/react-plot-axis/static/js/2.b35c4456.chunk.js"
+  },
+  {
+    "revision": "930d0db31cef01270d96",
+    "url": "/react-plot-axis/static/js/main.d19bd926.chunk.js"
+  },
+  {
+    "revision": "c45d6f74039b3fc9e936",
+    "url": "/react-plot-axis/static/js/runtime~main.176a1312.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-plot-axis/precache-manifest.637d8fb059cfc6bf71ee9354c9ab9bb6.js"
+  "/react-plot-axis/precache-manifest.d1b830314c3fed9b3d38ef10bca76f13.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-plot-axis/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-plot-axis/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
