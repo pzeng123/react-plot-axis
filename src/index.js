902e7723a9ced98ae563c6755fdee7c39a8f5bc8
jtCodes <jt234wq@gmail.com>
2020-02-17 13:37:51 -0500

    date x axis zooming in out works correctly for month and days

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
+import { format } from "date-fns";
 import { DateXAxis, DateVerticalLineGrid, YAxis, YAxisSlabGrid } from "./lib";
 import { HashRouter as Router, Route, Link } from "react-router-dom";
+import moment from "moment";
 
 // CSS
 import "./index.css";
@@ -12,49 +14,155 @@ class DateXAxisBundle extends Component {
     this.state = {
       width: 1000,
       height: 50,
-      minX: 0,
-      maxX: 100000000000,
+      minX: 1477836800000,
+      maxX: 1506780800000,
+      range: 1506780800000,
       tickPosition: "top"
     };
+    this.minX = 1277836800000;
+    this.maxX = 1506780800000;
+    this.range = 1506780800000;
+  }
+
+  // Given a date range in unix, return the common date components
+  // of the dates that falls within the date range
+  // ie: minDate = Feb 2, 2019 maxDate = Feb 28, 2019
+  // common components = Feb, 2019
+  getCommonDateComponents(minUnix, maxUnix) {
+    let minT = new Date(minUnix);
+    let maxT = new Date(maxUnix);
+
+    if (minT.getFullYear() === maxT.getFullYear()) {
+      if (minT.getMonth() === maxT.getMonth()) {
+        if (minT.getDate() === maxT.getDate()) {
+          if (minT.getHours() === maxT.getHours()) {
+            if (minT.getMinutes() === maxT.getMinutes()) {
+              if (minT.getSeconds() === maxT.getSeconds()) {
+                return format(minT, "YYYY/MMM/Do HH:mm:ss");
+              }
+              return format(minT, "YYYY/MMM/Do HH:mm");
+            }
+            return format(minT, "YYYY/MMM/Do HH");
+          }
+          return format(minT, "YYYY/MMM/Do");
+        }
+        return format(minT, "MMM, YYYY");
+      }
+      return format(minT, "YYYY");
+    }
+    return "Time";
+  }
+
+  generateXAxisTicks(minUnix, maxUnix, frameWidth) {
+    let tickPadding = 10;
+    let tickWidth = 40 + tickPadding;
+    let timeLengthInSecs = maxUnix - minUnix;
+    let ticksInUnix = [];
+    let curTick = null;
+    let minMoment = moment.unix(minUnix);
+
+    if (timeLengthInSecs <= 3600) {
+      // round down to nearest hour
+      let tickStartMinX = minUnix - (minUnix % 3600);
+      let tickIncrementInSecs = 300;
+
+      curTick = tickStartMinX;
+      ticksInUnix.push(curTick * 1000);
+      while (curTick <= maxUnix) {
+        curTick += tickIncrementInSecs;
+        ticksInUnix.push(curTick * 1000);
+      }
+    }
   }
 
   render() {
     let { width, height, minX, maxX, tickPosition } = this.state;
+    this.generateXAxisTicks(minX / 1000, maxX / 1000, width);
     return (
       <Fragment>
         <div>
           width:
-          <input type="range" min={800} max={1600} step={1}
+          <input
+            type="range"
+            min={800}
+            max={1600}
+            step={1}
             value={width}
-            onChange={(ev) => this.setState({ width: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ width: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div>
           height:
-          <input type="range" min={50} max={100} step={1}
+          <input
+            type="range"
+            min={50}
+            max={100}
+            step={1}
             value={height}
-            onChange={(ev) => this.setState({ height: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ height: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div>
           minX:
-          <input type="range" min={-100000000000} max={maxX} step={1}
+          <input
+            type="range"
+            min={this.minX}
+            max={this.maxX}
+            step={1}
             value={minX}
-            onChange={(ev) => this.setState({ minX: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ minX: Number.parseInt(ev.target.value) })
+            }
+            style={{ width: this.state.width }}
+          />
         </div>
         <div>
           maxX:
-          <input type="range" min={minX} max={100000000000} step={1}
+          <input
+            type="range"
+            min={this.minX}
+            max={this.maxX}
+            step={1}
             value={maxX}
-            onChange={(ev) => this.setState({ maxX: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ maxX: Number.parseInt(ev.target.value) })
+            }
+            style={{ width: this.state.width }}
+          />
+        </div>
+        <div>
+          range:
+          <input
+            type="range"
+            min={this.minX}
+            max={this.maxX}
+            step={1}
+            value={maxX}
+            onChange={ev =>
+              this.setState({
+                maxX: Number.parseInt(ev.target.value),
+                minX: Number.parseInt(ev.target.value) - 3600000
+              })
+            }
+            style={{ width: this.state.width }}
+          />
         </div>
         <div>
           tickPosition:
-          <select value={tickPosition}
-            onChange={(ev) => this.setState({ tickPosition: ev.target.value })}>
+          <select
+            value={tickPosition}
+            onChange={ev => this.setState({ tickPosition: ev.target.value })}
+          >
             <option value="top">top</option>
             <option value="bottom">bottom</option>
           </select>
         </div>
-        <DateXAxis width={width}
+        <DateXAxis
+          width={width}
           height={height}
           minX={minX}
           maxX={maxX}
@@ -65,6 +173,10 @@ class DateXAxisBundle extends Component {
           lineWidth={2}
           isItalic={true}
         />
+        <div>{this.getCommonDateComponents(minX, maxX)}</div>
+        <div>
+          {moment(minX).format()} {moment(maxX).format()}
+        </div>
       </Fragment>
     );
   }
@@ -88,31 +200,62 @@ class DateVerticalLineGridBundle extends Component {
       <Fragment>
         <div>
           width:
-          <input type="range" min={800} max={1600} step={1}
+          <input
+            type="range"
+            min={800}
+            max={1600}
+            step={1}
             value={width}
-            onChange={(ev) => this.setState({ width: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ width: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div>
           height:
-          <input type="range" min={100} max={500} step={1}
+          <input
+            type="range"
+            min={100}
+            max={500}
+            step={1}
             value={height}
-            onChange={(ev) => this.setState({ height: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ height: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div>
           minX:
-          <input type="range" min={-100000000000} max={maxX} step={1}
+          <input
+            type="range"
+            min={-100000000000}
+            max={maxX}
+            step={1}
             value={minX}
-            onChange={(ev) => this.setState({ minX: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ minX: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div>
           maxX:
-          <input type="range" min={minX} max={100000000000} step={1}
+          <input
+            type="range"
+            min={minX}
+            max={100000000000}
+            step={1}
             value={maxX}
-            onChange={(ev) => this.setState({ maxX: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ maxX: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div style={{ display: "flex", flexDirection: "column" }}>
-          <DateVerticalLineGrid width={width} height={height}
-            minX={minX} maxX={maxX}
+          <DateVerticalLineGrid
+            width={width}
+            height={height}
+            minX={minX}
+            maxX={maxX}
           />
           <DateXAxis
             width={width}
@@ -145,38 +288,71 @@ class YAxisBundle extends Component {
       <Fragment>
         <div>
           width:
-          <input type="range" min={50} max={100} step={1}
+          <input
+            type="range"
+            min={50}
+            max={100}
+            step={1}
             value={width}
-            onChange={(ev) => this.setState({ width: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ width: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div>
           height:
-          <input type="range" min={200} max={800} step={1}
+          <input
+            type="range"
+            min={200}
+            max={800}
+            step={1}
             value={height}
-            onChange={(ev) => this.setState({ height: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ height: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div>
           minY:
-          <input type="range" min={-10000} max={maxY} step={1}
+          <input
+            type="range"
+            min={-10000}
+            max={maxY}
+            step={1}
             value={minY}
-            onChange={(ev) => this.setState({ minY: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ minY: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div>
           maxY:
-          <input type="range" min={minY} max={10000} step={1}
+          <input
+            type="range"
+            min={minY}
+            max={10000}
+            step={1}
             value={maxY}
-            onChange={(ev) => this.setState({ maxY: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ maxY: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div>
           tickPosition:
-          <select value={tickPosition}
-            onChange={(ev) => this.setState({ tickPosition: ev.target.value })}>
+          <select
+            value={tickPosition}
+            onChange={ev => this.setState({ tickPosition: ev.target.value })}
+          >
             <option value="left">left</option>
             <option value="right">right</option>
           </select>
         </div>
-        <YAxis width={50} height={height}
-          minY={minY} maxY={maxY}
+        <YAxis
+          width={50}
+          height={height}
+          minY={minY}
+          maxY={maxY}
           tickPosition="left"
         />
       </Fragment>
@@ -191,7 +367,7 @@ class YAxisSlabGridBundle extends Component {
       width: 800,
       height: 400,
       minY: 0,
-      maxY: 10000,
+      maxY: 10000
     };
   }
 
@@ -201,35 +377,69 @@ class YAxisSlabGridBundle extends Component {
       <Fragment>
         <div>
           width:
-          <input type="range" min={400} max={1200} step={1}
+          <input
+            type="range"
+            min={400}
+            max={1200}
+            step={1}
             value={width}
-            onChange={(ev) => this.setState({ width: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ width: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div>
           height:
-          <input type="range" min={200} max={800} step={1}
+          <input
+            type="range"
+            min={200}
+            max={800}
+            step={1}
             value={height}
-            onChange={(ev) => this.setState({ height: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ height: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div>
           minY:
-          <input type="range" min={-10000} max={maxY} step={1}
+          <input
+            type="range"
+            min={-10000}
+            max={maxY}
+            step={1}
             value={minY}
-            onChange={(ev) => this.setState({ minY: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ minY: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div>
           maxY:
-          <input type="range" min={minY} max={10000} step={1}
+          <input
+            type="range"
+            min={minY}
+            max={10000}
+            step={1}
             value={maxY}
-            onChange={(ev) => this.setState({ maxY: Number.parseInt(ev.target.value) })} />
+            onChange={ev =>
+              this.setState({ maxY: Number.parseInt(ev.target.value) })
+            }
+          />
         </div>
         <div style={{ display: "flex" }}>
-          <YAxis width={50} height={height}
-            minY={minY} maxY={maxY}
+          <YAxis
+            width={50}
+            height={height}
+            minY={minY}
+            maxY={maxY}
             tickPosition="right"
           />
-          <YAxisSlabGrid width={width} height={height}
-            minY={minY} maxY={maxY}
+          <YAxisSlabGrid
+            width={width}
+            height={height}
+            minY={minY}
+            maxY={maxY}
           />
         </div>
       </Fragment>
@@ -237,7 +447,7 @@ class YAxisSlabGridBundle extends Component {
   }
 }
 
-const App = (props) => {
+const App = props => {
   return (
     <Router>
       <nav className="app">
@@ -247,11 +457,15 @@ const App = (props) => {
         <Link to="/YAxisSlabGrid">YAxisSlabGridBundle</Link>
       </nav>
       <Route path="/DateXAxis" exact component={DateXAxisBundle} />
-      <Route path="/DateVerticalLineGrid" exact component={DateVerticalLineGridBundle} />
+      <Route
+        path="/DateVerticalLineGrid"
+        exact
+        component={DateVerticalLineGridBundle}
+      />
       <Route path="/YAxis" exact component={YAxisBundle} />
       <Route path="/YAxisSlabGrid" exact component={YAxisSlabGridBundle} />
     </Router>
   );
-}
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/lib/DateXAxis.js
+++ src/lib/DateXAxis.js
@@ -4,11 +4,19 @@ import PropTypes from "prop-types";
 import { bisect_left, bisect_right } from "bisect";
 import { toDomXCoord_Linear, generateDateGrids } from "plot-utils";
 import { format } from "date-fns";
+import moment from "moment";
+
+const HOUR_IN_SECS = 3600;
+const DAY_IN_SECS = 86400;
+const MONTH_IN_SECS = 2592000;
 
 class DateXAxis extends PureComponent {
   constructor(props) {
     super(props);
     this.ref = React.createRef();
+    this.curDate = null;
+    this.curMonth = null;
+    this.curYear = null;
   }
 
   render() {
@@ -19,8 +27,7 @@ class DateXAxis extends PureComponent {
         width={width}
         height={height}
         style={{ width: width, height: height, display: "block" }}
-      >
-      </canvas>
+      ></canvas>
     );
   }
 
@@ -45,31 +52,41 @@ class DateXAxis extends PureComponent {
       strokeStyle,
       lineWidth
     } = this.props;
-    this.draw_memo = this.draw_memo || { validFromDiff: 0, validToDiff: -1, rangeMinX: 0, rangeMaxX: -1 };
+    this.draw_memo = this.draw_memo || {
+      validFromDiff: 0,
+      validToDiff: -1,
+      rangeMinX: 0,
+      rangeMaxX: -1
+    };
     let memo = this.draw_memo;
     let diffX = maxX - minX;
 
     // Generate grids, labels and bitmaps in cache
-    if (memo.validFromDiff > diffX ||
-      diffX > memo.validToDiff ||
-      memo.rangeMinX > minX ||
-      maxX > memo.rangeMaxX
-    ) {
-      memo.rangeMinX = minX - 10 * diffX;
-      memo.rangeMaxX = maxX + 10 * diffX;
-      let { grids, validFromDiff, validToDiff } = generateDateGrids(minX, maxX, memo.rangeMinX, memo.rangeMaxX);
-      memo.validFromDiff = validFromDiff;
-      memo.validToDiff = validToDiff;
-      memo.grids = grids;
-      memo.gridLabels = this.getGridLabels(grids);
-    }
+    // if (memo.validFromDiff > diffX ||
+    //   diffX > memo.validToDiff ||
+    //   memo.rangeMinX > minX ||
+    //   maxX > memo.rangeMaxX
+    // ) {
+    //   memo.rangeMinX = minX - 10 * diffX;
+    //   memo.rangeMaxX = maxX + 10 * diffX;
+    //   let { validFromDiff, validToDiff } = generateDateGrids(minX, maxX, memo.rangeMinX, memo.rangeMaxX);
+    //   memo.validFromDiff = validFromDiff;
+    //   memo.validToDiff = validToDiff;
+    //   memo.grids = this.generateXAxisTicks(minX / 1000, maxX / 1000, width);
+    //   memo.gridLabels = this.getGridLabels(memo.grids);
+    //   // console.log(memo.grids)
+    // }
 
     // Filter
-    let startIndex = Math.max(0, bisect_right(memo.grids, minX));
-    let endIndex = Math.min(memo.grids.length - 1, bisect_left(memo.grids, maxX));
-
-    let domXs = memo.grids.slice(startIndex, endIndex + 1).map((x) => toDomXCoord_Linear(width, minX, maxX, x));
-    let gridLabels = memo.gridLabels.slice(startIndex, endIndex + 1);
+    let grids = this.generateXAxisTicks(minX / 1000, maxX / 1000, width);
+    let gridLabels = this.getGridLabels(grids);
+    let startIndex = Math.max(0, bisect_right(grids, minX));
+    let endIndex = Math.min(grids.length - 1, bisect_left(grids, maxX));
+    // console.log(moment(minX), moment(maxX), gridLabels, startIndex, endIndex, minX, maxX, grids)
+    let domXs = grids
+      .slice(startIndex, endIndex + 1)
+      .map(x => toDomXCoord_Linear(width, minX, maxX, x));
+    gridLabels = gridLabels.slice(startIndex, endIndex + 1);
 
     // Plot
     let canvas = this.ref.current;
@@ -77,43 +94,195 @@ class DateXAxis extends PureComponent {
     ctx.clearRect(0, 0, width, height);
 
     if (fontSize && fontWeight) {
-      this.textPlot(ctx, width, height, domXs, gridLabels, fontSize, fontWeight, isItalic);
+      this.textPlot(
+        ctx,
+        width,
+        height,
+        domXs,
+        gridLabels,
+        fontSize,
+        fontWeight,
+        isItalic
+      );
     } else {
       this.textPlot(ctx, width, height, domXs, gridLabels, 12, 400, isItalic);
     }
-    this.ticPlot(ctx, width, height, domXs, tickPosition, strokeStyle, lineWidth);
+    this.ticPlot(
+      ctx,
+      width,
+      height,
+      domXs,
+      tickPosition,
+      strokeStyle,
+      lineWidth
+    );
+  }
+
+  generateXAxisTicks(minUnix, maxUnix, frameWidth) {
+    let tickPadding = 10;
+    let tickWidth = 40 + tickPadding;
+    let timeLengthInSecs = maxUnix - minUnix;
+    let ticksInUnix = [];
+    let curTick = null;
+    let minMoment = moment.unix(minUnix);
+    let maxMoment = moment.unix(maxUnix);
+    let tickIncrementInSecs = 300;
+    let timeLengthInHours = Math.round(timeLengthInSecs / 3600);
+    let timeLengthInDays = maxMoment.diff(minMoment, "days");
+
+    // round down to nearest hour
+    let tickStartMinX = minMoment.startOf("month").unix();
+    curTick = tickStartMinX;
+    //timeLengthInHours * tickIncrementInSecs;
+    tickIncrementInSecs = 2592000;
+
+    // if (timeLengthInDays > 356 && timeLengthInDays < 800) {
+    //   // tickIncrementInSecs = 600
+    //   tickIncrementInSecs = 2592000 * 2;
+    // } else if (timeLengthInDays >= 800) {
+    //   tickIncrementInSecs = 2592000 * 3;
+    // }
+
+    if (timeLengthInDays <= 30) {
+      tickIncrementInSecs = DAY_IN_SECS;
+    } else if (timeLengthInDays > 30 && timeLengthInDays <= 50) {
+      tickIncrementInSecs = DAY_IN_SECS * 2;
+    } else if (timeLengthInDays > 50 && timeLengthInDays <= 100) {
+      tickIncrementInSecs = DAY_IN_SECS * 3;
+    } else if (timeLengthInDays > 100 && timeLengthInDays <= 180) {
+      tickIncrementInSecs = DAY_IN_SECS * 4;
+    } else if (timeLengthInDays > 180 && timeLengthInDays <= 250) {
+      tickIncrementInSecs = DAY_IN_SECS * 8;
+    } else if (timeLengthInDays > 250 && timeLengthInDays <= 250) {
+      tickIncrementInSecs = DAY_IN_SECS * 8;
+    } 
+
+    console.log(timeLengthInDays);
+    // console.log(timeLengthInDays)
+    while (curTick <= maxUnix) {
+      let prevTick = curTick - tickIncrementInSecs;
+      let nextTick = curTick + tickIncrementInSecs;
+      let prevTickDate = moment.unix(prevTick).date();
+      let curTickDate = moment.unix(curTick).date();
+      let nextTickDate = moment.unix(nextTick).date();
+      let prevTickMonth = moment.unix(prevTick).month();
+      let curTickMonth = moment.unix(curTick).month();
+      let nextTickMonth = moment.unix(nextTick).month();
+      let prevTickYear = moment.unix(prevTick).year();
+      let nextTickYear = moment.unix(nextTick).year();
+      let curTickYear = moment.unix(curTick).year();
+
+
+   if (prevTickYear !== curTickYear) {
+        ticksInUnix.push(
+          moment
+            .unix(curTick)
+            .startOf("year")
+            .unix() * 1000
+        );
+      } else if (prevTickMonth !== curTickMonth) {
+        ticksInUnix.push(
+          moment
+            .unix(curTick)
+            .startOf("month")
+            .unix() * 1000
+        );
+      } 
+      else if (prevTickDate !== nextTickDate && curTickMonth === nextTickMonth) {
+        ticksInUnix.push(moment.unix(curTick).startOf('day').unix() * 1000)
+      }
+      else if (curTickMonth === nextTickMonth) {
+        ticksInUnix.push(curTick * 1000);
+      }
+
+      // if (curTickDate === prevTickDate && curTickDate === nextTickDate) {
+      //   // nextTick -= nextTick % 86400
+      //   // console.log(moment.unix(nextTick - nextTick % 86400).format(), moment.unix(nextTick).format())
+      //   // nextTick = moment.unix(nextTick).startOf('day').unix()
+      //   // console.log(moment.unix(nextTick).format(), moment.unix(nextTick).startOf('day').format())
+      //   // curTick = nextTick
+      //   // ticksInUnix.push(curTick * 1000)
+      //   ticksInUnix.push(curTick * 1000)
+      // } else if (curTickDate !== prevTickDate) {
+      //   ticksInUnix.push(moment.unix(curTick).startOf('day').unix() * 1000)
+      // }
+      // ticksInUnix.push(curTick * 1000)
+      curTick = nextTick;
+      // console.log(moment.unix(curTick).format(), moment.unix(nextTick).format())
+    }
+
+    return ticksInUnix;
   }
 
   getGridLabels(grids) {
     let labels = [];
     let t = new Date();
+    console.log(grids);
     for (let grid of grids) {
       t.setTime(grid);
+      // console.log(t)
       labels.push(this.getMeaningfulDateField(t));
     }
     return labels;
   }
 
   getMeaningfulDateField(d) {
-    if (d.getMilliseconds() === 0) {
-      if (d.getSeconds() === 0) {
-        if (d.getMinutes() === 0) {
-          if (d.getHours() === 0) {
-            if (d.getDate() === 1) {
-              if (d.getMonth() === 0) {
-                return format(d, "YYYY");
-              }
-              return format(d, "MMM");
-            }
-            return format(d, "Do");
-          }
-          return format(d, "HH:00");
-        }
-        return format(d, "HH:mm");
-      }
-      return format(d, "HH:mm:ss");
+    // console.log(format(d,"MMM, Do, YYYY, h:MM "), d.getHours(), d.getMonth())
+
+    // if (this.curMonth !== d.getMonth()) {
+    //   this.curMonth = d.getMonth()
+    //   return format(d, "MMM");
+    // }
+
+    // if (this.curDate !== d.getDate()) {
+    //   this.curDate = d.getDate()
+    //   return format(d, "Do");
+    // }
+
+    if (
+      d.getMonth() === 0 &&
+      d.getDate() === 1 &&
+      d.getHours() === 0 &&
+      d.getMinutes() === 0 &&
+      d.getSeconds() === 0
+    ) {
+      return format(d, "YYYY");
     }
-    return format(d, "ss.SSS");
+
+    if (
+      d.getDate() === 1 &&
+      d.getHours() === 0 &&
+      d.getMinutes() === 0 &&
+      d.getSeconds() === 0
+    ) {
+      return format(d, "MMM");
+    }
+
+    if (d.getHours() === 0 && d.getMinutes() === 0 && d.getSeconds() === 0) {
+      return format(d, "D");
+    }
+    // console.log(format(d, "MMM, Do, YYYY, HH:MM "), d.getHours(), d.getMonth())
+    return d.getHours() + ":" + d.getMinutes();
+    // if (d.getMilliseconds() === 0) {
+    //   if (d.getSeconds() === 0) {
+    //     if (d.getMinutes() === 0) {
+    //       if (d.getHours() === 0) {
+    //         if (d.getDate() === 1) {
+    //           if (d.getMonth() === 0) {
+    //             console.log(d)
+    //             return format(d, "YYYY");
+    //           }
+    //           return format(d, "MMM, YYYY");
+    //         }
+    //         return format(d, "Do");
+    //       }
+    //       return d.getHours() + ":" + d.getMinutes();
+    //     }
+    //     return d.getHours() + ":" + d.getMinutes();
+    //   }
+    //   return format(d, "HH:mm:ss");
+    // }
+    // return format(d, "ss.SSS");
   }
 
   textPlot(ctx, width, height, domXs, texts, fontSize, fontWeight, isItalic) {
@@ -124,7 +293,7 @@ class DateXAxis extends PureComponent {
       ctx.font = "italic " + fontWeight + " " + fontSize + "px MuseoSans, Sans";
     } else {
       ctx.font = fontWeight + " " + fontSize + "px MuseoSans, Sans";
-    } 
+    }
 
     for (let i = 0; i < domXs.length; i++) {
       let text = texts[i];
@@ -136,13 +305,13 @@ class DateXAxis extends PureComponent {
 
   ticPlot(ctx, width, height, domXs, tickPosition, strokeStyle, lineWidth) {
     if (strokeStyle) {
-      ctx.strokeStyle = strokeStyle
+      ctx.strokeStyle = strokeStyle;
     }
 
     if (lineWidth) {
-      ctx.lineWidth = lineWidth
+      ctx.lineWidth = lineWidth;
     }
-    
+
     switch (tickPosition) {
       case "top":
       default:
@@ -174,7 +343,7 @@ DateXAxis.propTypes = {
   maxX: PropTypes.number.isRequired,
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
-  tickPosition: PropTypes.string.isRequired,
-}
+  tickPosition: PropTypes.string.isRequired
+};
 
 export default DateXAxis;
